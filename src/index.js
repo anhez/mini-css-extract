04cdf75124152b9c95738f46e9f0488c81652234
anhez <anhtd.dev@gmail.com>
2022-08-24 09:32:10 +0700

    Issue

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./index.scss";
+function component() {
+	const element = document.createElement("div");
+	element.innerHTML = "Hello";
+
+	return element;
+}
+
+document.body.appendChild(component());
